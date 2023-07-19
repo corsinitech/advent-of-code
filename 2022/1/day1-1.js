5f379e32e1805d15466df965ba59b36f9382ba1f
Gabe Corsini <gabe.corsini@gmail.com>
2023-07-19 00:28:50 -0400

    Initial commit + Add Day 1 part 1 and 2 submission

---
+++ 2022/1/day1-1.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const {execSync} = require('child_process');
+
+let bigDictElves = {};
+let sortedElvesByCalorie = [];
+
+let calorieInventory = fs.readFileSync('./input.txt').toString().split('\n');
+
+console.log(calorieInventory);
+
+function elfDictInitializer() {
+    elfIndex = 0;
+    for (let i = 0; i < calorieInventory.length; i++) {
+        console.log('Current item: ' + calorieInventory[i]);
+        if (calorieInventory[i] == ''){
+            console.log('Adding elf number ' + (elfIndex + 1) + ' to object...');
+            bigDictElves[`elf${elfIndex + 1}`] = 0;
+            elfIndex += 1;
+        }
+        //execSync('sleep 1');
+        
+    }
+}
+
+function calorieCalculation() {
+    elfIndex = 0;
+    for (let i = 0; i < calorieInventory.length; i++) {
+        if (calorieInventory[i] !== '') {
+            bigDictElves[`elf${elfIndex + 1}`] += parseInt(calorieInventory[i]);
+        } else {
+            elfIndex += 1;
+        }
+    }
+}
+
+function sortElves() {
+    let items = Object.keys(bigDictElves).map(function(key) {
+        return [key, bigDictElves[key]];
+    });
+
+    items.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+
+    return items.slice(0, 5);
+}
+
+elfDictInitializer();
+calorieCalculation();
+sortedElvesByCalorie = sortElves();
+console.log(bigDictElves);
+console.log(sortedElvesByCalorie);
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# advent-of-code
+# Advent of Code!
+
+A repository for all my AoC challenges. Some of this code probbably sucks. If
+time permits, I'd like to go back and rewrite some of these as I improve.
+
+### 2022
+- [x] Day 1
+- [ ] Day 2
+- [ ] Day 3
+- [ ] Day 4
+- [ ] Day 5
+- [ ] Day 6
+- [ ] Day 7
+- [ ] Day 8
+- [ ] Day 9
+- [ ] Day 10
+- [ ] Day 11
+- [ ] Day 12
+- [ ] Day 13
+- [ ] Day 14
+- [ ] Day 15
+- [ ] Day 16
+- [ ] Day 17
+- [ ] Day 18
+- [ ] Day 19
+- [ ] Day 20
+- [ ] Day 21
+- [ ] Day 22
+- [ ] Day 23
+- [ ] Day 24
+- [ ] Day 25
